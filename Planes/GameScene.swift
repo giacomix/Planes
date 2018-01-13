bc350b15c0a4907c8dc5a786814ae0fbab109231
Tilly Jack Perez <jackmac@macbook-air-di-giacomo.local>
2018-01-13 19:37:27 +0100

    prima versione include actions

--- Planes/GameScene.swift
+++ Planes/GameScene.swift
@@ -9,72 +9,86 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ship = SKSpriteNode()
+    var actionMoveUp = SKAction()
+    var actionMoveDown = SKAction()
+    let shipCategory = 0x1 << 1
+    let obstacleCategory = 0x1 << 2
+    
+    
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        self.backgroundColor = SKColor.white
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.addShip()
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+    }
+    
+    func addShip() {
+        // Inizializa una nave
+        ship = SKSpriteNode(imageNamed: "spaceship")
+        ship.setScale(1.5)
+        ship.zRotation = CGFloat(-Double.pi / 2)
+        
+        // aggiungiamo physics body for collision detection
+        
+        ship.physicsBody = SKPhysicsBody(circleOfRadius: ship.size.width)
+        ship.physicsBody?.categoryBitMask = UInt32(shipCategory)
+        ship.physicsBody?.isDynamic = true
+        ship.physicsBody?.contactTestBitMask = UInt32(obstacleCategory)
+        ship.physicsBody?.collisionBitMask = 0
+        ship.name = "ship"
+        ship.position = CGPoint(x: 0, y: 0)
+        
+        self.addChild(ship)
+        
+        actionMoveUp = SKAction.move(by: CGVector(dx: 0, dy: 30), duration: 0.2)
+        actionMoveDown = SKAction.move(by: CGVector(dx: 0, dy: -30), duration: 0.2)
+        
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+       
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        print("il primo touch")
+        
+        for touch: AnyObject in touches {
+            let location  = touch.location(in: self)
+            if location.y > ship.position.y {
+               ship.run(actionMoveUp)
+            } else {
+                ship.run(actionMoveDown)
+            }
         }
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        print("posizione del touch")
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        print("touch finalizzato")
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
